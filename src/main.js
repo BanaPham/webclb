82536ee88b900f0dc5866663258ff97d342a7a0d
BanaPham <phamlananha1anh@gmail.com>
2025-02-18 22:40:44 +0700

    dfs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Navigo from "navigo";
 import Trangchu, {Inid, Kenhdangky, Shorts} from "./you.js"
 
 const router = new Navigo("/", {
-    linksSelector: "button",
+    linksSelector: "a",
+});
+
+document.addEventListener("click", (event) => {
+    const button = event.target.closest("button"); 
+    if (button) {
+        const link = button.querySelector("a"); 
+        if (link) {
+            event.preventDefault(); 
+            router.navigate(link.getAttribute("href")); 
+        }
+    }
 });
 
 router.on("/", function(){
