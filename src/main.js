1117ec0be38b130a6c6d000b598706ccdf3b5c42
BanaPham <phamlananha1anh@gmail.com>
2025-02-19 13:34:33 +0700

    sdffs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import Trangchu, {Kenhdangky, Shorts} from "./you.js"
 
 const router = new Navigo('/');
 
+document.addEventListener("click", (event) => {
+    const button = event.target.closest("button"); 
+    if (button) {
+        const link = button.querySelector("a"); 
+        if (link) {
+            event.preventDefault(); 
+            router.navigate(link.getAttribute("href")); 
+        }
+    }
+});
+
 router.on("/", function(){
     Trangchu()
 });
--- src/you.js
+++ src/you.js
@@ -46,6 +46,5 @@ function Kenhdangky()
     });
 }
 
-
 export default Trangchu
 export {Shorts, Kenhdangky}
